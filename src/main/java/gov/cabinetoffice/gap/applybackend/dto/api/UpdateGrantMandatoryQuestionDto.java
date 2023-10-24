42a2fe9ee6c5ccac15f89b904a71d5a9b658c0e2
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2023-10-24 13:40:06 +0100

    add validation to mandatory questions

--- src/main/java/gov/cabinetoffice/gap/applybackend/dto/api/UpdateGrantMandatoryQuestionDto.java
+++ src/main/java/gov/cabinetoffice/gap/applybackend/dto/api/UpdateGrantMandatoryQuestionDto.java
@@ -5,7 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Data
@@ -16,57 +21,73 @@ public class UpdateGrantMandatoryQuestionDto {
 
     private UUID submissionId;
 
-    //    @NotBlank(message = "You must enter an answer")
-//    @Size(max = 250, message = "Organisation name must be 250 characters or less")
-//    @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
-//            message = "Organisation name must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
-    private String name;
+    private Optional<
+                @NotBlank(message = "You must enter an answer")
+                @Size(max = 250, message = "Organisation name must be 250 characters or less")
+                @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
+                        message = "Organisation name must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
+            String> name;
 
-    //    @NotBlank(message = "You must enter an answer for the address line 1")
-//    @Size(max = 250, message = "Address line 1 must be 250 characters or less")
-//    @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
-//            message = "Address line 1 must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
-    private String addressLine1;
+    private Optional<
+                @NotBlank(message = "You must enter an answer for the address line 1")
+                @Size(max = 250, message = "Address line 1 must be 250 characters or less")
+                @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
+                        message = "Address line 1 must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
+            String> addressLine1;
 
-    //    @Size(max = 250, message = "Address line 2 must be 250 characters or less")
-//    @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
-//            message = "Address line 2 must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
-    private String addressLine2;
+    private Optional<
+                @Size(max = 250, message = "Address line 2 must be 250 characters or less")
+                @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
+                        message = "Address line 2 must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
+            String> addressLine2;
 
-    //    @NotBlank(message = "You must enter an answer for the city")
-//    @Size(max = 250, message = "City must be 250 characters or less")
-//    @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
-//            message = " City must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
-    private String city;
+    private Optional<
+                @NotBlank(message = "You must enter an answer for the city")
+                @Size(max = 250, message = "City must be 250 characters or less")
+                @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
+                        message = " City must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
+            String> city;
 
-    //    @Size(max = 250, message = "County must be 250 characters or less")
-//    @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
-//            message = "County must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
-    private String county;
 
-    //    @NotBlank(message = "You must enter an answer for the postcode")
-//    @Size(max = 8, message = "Postcode must be 8 characters or less")
-//    @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
-//            message = "Postcode must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
-    private String postcode;
+    private Optional<
+                @Size(max = 250, message = "County must be 250 characters or less")
+                @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
+                        message = "County must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
+            String> county;
 
-    //    @Size(max = 250, message = "Charity commission number must be 250 characters or less")
-//    @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
-//            message = "Charity commission number must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
-    private String charityCommissionNumber;
 
-    //    @Size(max = 250, message = "Companies house number must be 250 characters or less")
-//    @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
-//            message = "Companies house must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
-    private String companiesHouseNumber;
+    private Optional<
+                @NotBlank(message = "You must enter an answer for the postcode")
+                @Size(max = 8, message = "Postcode must be 8 characters or less")
+                @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
+                        message = "Postcode must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
+            String> postcode;
 
-    //    @NotBlank(message = "You must select an answer")
-    private String orgType;
+    private Optional<
+                @Size(max = 250, message = "Charity commission number must be 250 characters or less")
+                @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
+                        message = "Charity commission number must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
+            String> charityCommissionNumber;
 
-    //    @NotBlank(message = "You must enter an answer")
-//    @Pattern(regexp = "^[0-9]+[.][0-9]{1,2}$",message = "Funding amount must only contain numbers, and decimal(max 2) will need to be split with a dot")
-    private String fundingAmount;
+    private Optional<
+                @Size(max = 250, message = "Companies house number must be 250 characters or less")
+                @Pattern(regexp = "^(?![\\s\\S])|^[a-zA-Z0-9\\s',-]+$",
+                        message = "Companies house must only use letters, numbers, and special characters such as hyphens, spaces and apostrophes")
+            String> companiesHouseNumber;
 
-    //    @NotEmpty(message = "You must select an answer")
-    private List<String> fundingLocation;
+
+    private Optional<
+                @NotBlank(message = "You must select an answer")
+            String> orgType;
+
+
+    private Optional<
+                @Pattern(regexp = "^\\d+$",message = "Funding amount must only contain whole numbers")
+            String> fundingAmount;
+
+
+    private Optional<
+                @NotEmpty(message = "You must select an answer")
+            List<String>> fundingLocation;
+
 }
