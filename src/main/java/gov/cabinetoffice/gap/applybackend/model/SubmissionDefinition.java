1c5d7e70bcece1bb491da657ac355630d4d5a500
GavCookCO <99668051+GavCookCO@users.noreply.github.com>
2023-10-31 13:17:27 +0000

    Numerous (#36)
    - Added code to replace the ESSENTIAL section with ORGANISATION_DETAILS and FUNDING_DETAILS sections in the submission definition for >V1 schemes.
    - Removed the transform application definition to submission definition functionality from the POJO and moved it to a service layer class.
    - Removed the now-redundant test for that functionality and re-created in the service layer test class.
    - Added code to actually log the exceptions that are handled in the Controller exception handler (how did we miss this??)
    - Refactored formatting in some places.

--- src/main/java/gov/cabinetoffice/gap/applybackend/model/SubmissionDefinition.java
+++ src/main/java/gov/cabinetoffice/gap/applybackend/model/SubmissionDefinition.java
@@ -1,8 +1,5 @@
 package gov.cabinetoffice.gap.applybackend.model;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import gov.cabinetoffice.gap.applybackend.enums.SubmissionSectionStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,16 +15,4 @@ import java.util.List;
 public class SubmissionDefinition {
     @Builder.Default
     private List<SubmissionSection> sections = new ArrayList<>();
-
-    public static SubmissionDefinition transformApplicationDefinitionToSubmissionOne(ApplicationDefinition applicationDefinition) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String applicationDefinitionJson = objectMapper.writeValueAsString(applicationDefinition);
-        SubmissionDefinition definition = objectMapper.readValue(applicationDefinitionJson, SubmissionDefinition.class);
-        definition.getSections().forEach(section -> {
-                    final SubmissionSectionStatus status = section.getSectionId().equals("ELIGIBILITY") ? SubmissionSectionStatus.NOT_STARTED : SubmissionSectionStatus.CANNOT_START_YET;
-                    section.setSectionStatus(status);
-                }
-        );
-        return definition;
-    }
 }
--- src/main/java/gov/cabinetoffice/gap/applybackend/web/GrantMandatoryQuestionsController.java
+++ src/main/java/gov/cabinetoffice/gap/applybackend/web/GrantMandatoryQuestionsController.java
@@ -103,16 +103,17 @@ public class GrantMandatoryQuestionsController {
         final GrantMandatoryQuestions grantMandatoryQuestions = grantMandatoryQuestionService.getGrantMandatoryQuestionById(mandatoryQuestionId, jwtPayload.getSub());
 
         grantMandatoryQuestionMapper.mapUpdateGrantMandatoryQuestionDtoToGrantMandatoryQuestion(mandatoryQuestionDto, grantMandatoryQuestions);
-        if(mandatoryQuestionDto.getSubmissionId()!=null){
+        if (mandatoryQuestionDto.getSubmissionId() != null) {
             final Submission submission = submissionService.getSubmissionFromDatabaseBySubmissionId(jwtPayload.getSub(), mandatoryQuestionDto.getSubmissionId());
             grantMandatoryQuestions.setSubmission(submission);
         }
+
         if (grantMandatoryQuestions.getStatus().equals(GrantMandatoryQuestionStatus.NOT_STARTED)) {
             grantMandatoryQuestions.setStatus(GrantMandatoryQuestionStatus.IN_PROGRESS);
         }
+
         grantMandatoryQuestions.setLastUpdatedBy(applicant);
         grantMandatoryQuestions.setLastUpdated(Instant.now());
-
 
         grantMandatoryQuestionService.updateMandatoryQuestion(grantMandatoryQuestions);
 
@@ -120,6 +121,4 @@ public class GrantMandatoryQuestionsController {
 
         return ResponseEntity.ok(grantMandatoryQuestionService.generateNextPageUrl(url, mandatoryQuestionId));
     }
-
-
 }
