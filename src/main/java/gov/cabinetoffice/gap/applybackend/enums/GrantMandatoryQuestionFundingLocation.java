117a6d64cb3ba8e1fc875d7120b0031d28c458e1
kiramarstonTCO <kira.marston@cabinetoffice.gov.uk>
2023-11-07 10:54:51 +0000

    TMI2-328 - Updated funding locations list

--- src/main/java/gov/cabinetoffice/gap/applybackend/enums/GrantMandatoryQuestionFundingLocation.java
+++ src/main/java/gov/cabinetoffice/gap/applybackend/enums/GrantMandatoryQuestionFundingLocation.java
@@ -3,20 +3,20 @@ package gov.cabinetoffice.gap.applybackend.enums;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum GrantMandatoryQuestionFundingLocation {
-    NORTH_EAST_ENGLAND("North East England"),
-    NORTH_WEST_ENGLAND("North West England"),
+    NORTH_EAST_ENGLAND("North East (England)"),
+    NORTH_WEST_ENGLAND("North West (England)"),
     YORKSHIRE_AND_THE_HUMBER("Yorkshire and the Humber"),
     EAST_MIDLANDS_ENGLAND("East Midlands (England)"),
-    WEST_MIDLANDS("West Midlands"),
+    WEST_MIDLANDS("West Midlands (England)"),
     EAST_ENGLAND("East England"),
     LONDON("London"),
-    SOUTH_EAST_ENGLAND("South East England"),
-    SOUTH_WEST_ENGLAND("South West England"),
+    SOUTH_EAST_ENGLAND("South East (England)"),
+    SOUTH_WEST_ENGLAND("South West (England)"),
     MIDLANDS("Midlands"),
     SCOTLAND("Scotland"),
     WALES("Wales"),
     NORTHERN_IRELAND("Northern Ireland"),
-    OUTSIDE_UK("Outside UK");
+    OUTSIDE_UK("Outside of the UK");
 
     private final String name;
 
@@ -45,9 +45,3 @@ public enum GrantMandatoryQuestionFundingLocation {
     }
 
 }
-
-
-
-
-
-
