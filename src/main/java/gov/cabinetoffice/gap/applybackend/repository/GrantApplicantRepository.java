8db8535ed3d1f891a1551f1af9112afe8521e3e3
dominicwest <101722961+dominicwest@users.noreply.github.com>
2023-11-13 15:42:10 +0000

    GAP-2295: Create new user in middleware if valid jwt but no applicant exists (#55)

--- src/main/java/gov/cabinetoffice/gap/applybackend/repository/GrantApplicantRepository.java
+++ src/main/java/gov/cabinetoffice/gap/applybackend/repository/GrantApplicantRepository.java
@@ -2,9 +2,16 @@ package gov.cabinetoffice.gap.applybackend.repository;
 
 import gov.cabinetoffice.gap.applybackend.model.GrantApplicant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface GrantApplicantRepository extends JpaRepository<GrantApplicant, Long> {
     Optional<GrantApplicant> findByUserId(String userid);
+
+    boolean existsByUserId(String userId);
+
+
+
 }
