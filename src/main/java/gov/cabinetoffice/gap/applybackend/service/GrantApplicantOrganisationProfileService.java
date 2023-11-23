fe6a5c36e4c8997caf86da017615d88abc697486
dominicwest <dominic.west@cabinetoffice.gov.uk>
2023-11-23 15:00:53 +0000

    GAP-2251: Is org profile complete endpoint

--- src/main/java/gov/cabinetoffice/gap/applybackend/service/GrantApplicantOrganisationProfileService.java
+++ src/main/java/gov/cabinetoffice/gap/applybackend/service/GrantApplicantOrganisationProfileService.java
@@ -1,5 +1,6 @@
 package gov.cabinetoffice.gap.applybackend.service;
 
+import gov.cabinetoffice.gap.applybackend.enums.GrantApplicantOrganisationType;
 import gov.cabinetoffice.gap.applybackend.exception.NotFoundException;
 import gov.cabinetoffice.gap.applybackend.model.GrantApplicant;
 import gov.cabinetoffice.gap.applybackend.model.GrantApplicantOrganisationProfile;
@@ -36,5 +37,20 @@ public class GrantApplicantOrganisationProfileService {
 
         grantApplicantService.saveApplicant(applicant);
         return savedProfile;
+    }
+
+    public Boolean isOrganisationComplete(final String sub) {
+        final GrantApplicant applicant = grantApplicantService.getApplicantById(sub);
+        final GrantApplicantOrganisationProfile profile = applicant.getOrganisationProfile();
+        if (profile.getLegalName() == null || profile.getLegalName().isEmpty()) return false;
+        if (profile.getAddressLine1() == null || profile.getAddressLine1().isEmpty()) return false;
+        if (profile.getTown() == null || profile.getTown().isEmpty()) return false;
+        if (profile.getPostcode() == null || profile.getPostcode().isEmpty()) return false;
+        if (profile.getType() == null) return false;
+        if (profile.getType() != GrantApplicantOrganisationType.INDIVIDUAL && profile.getType() != GrantApplicantOrganisationType.NON_LIMITED_COMPANY) {
+            if (profile.getCompaniesHouseNumber() == null || profile.getCompaniesHouseNumber().isEmpty()) return false;
+            if (profile.getCharityCommissionNumber() == null || profile.getCharityCommissionNumber().isEmpty()) return false;
+        }
+        return true;
     }
 }
