8563ed1b51b0f4ac5c0c94023a40cdaab521cd3c
Connor Macqueen <138442814+ConnorTCO@users.noreply.github.com>
2023-12-07 15:43:57 +0000

    Logging submission publication, creation, and updates log event log service (#76)

--- src/main/java/gov/cabinetoffice/gap/applybackend/ApplyBackendApplication.java
+++ src/main/java/gov/cabinetoffice/gap/applybackend/ApplyBackendApplication.java
@@ -2,10 +2,12 @@ package gov.cabinetoffice.gap.applybackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ComponentScan(basePackages = "gov.cabinetoffice.gap")
 public class ApplyBackendApplication {
 
 	public static void main(String[] args) {
