23dd671e32cd9f50cdda8bde83b54efb637eeb3a
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2024-04-25 15:04:54 +0100

    GAP-2422: add custom RequestLoggingFilter

--- src/main/java/gov/cabinetoffice/gap/applybackend/security/filters/CustomRequestLoggingFilter.java
+++ src/main/java/gov/cabinetoffice/gap/applybackend/security/filters/CustomRequestLoggingFilter.java
@@ -1,21 +1,39 @@
 package gov.cabinetoffice.gap.applybackend.security.filters;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.AbstractRequestLoggingFilter;
-import javax.servlet.http.HttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Set;
+
+@Component
 public class CustomRequestLoggingFilter extends AbstractRequestLoggingFilter {
+
+    private final Set<String> excludedUrls = Set.of("/health");
+
+    public CustomRequestLoggingFilter() {
+        this.setIncludeHeaders(true);
+        this.setIncludeQueryString(true);
+        this.setMaxPayloadLength(10000);
+        this.setIncludePayload(true);
+    }
+
+    @Override
+    protected boolean shouldLog(HttpServletRequest request) {
+        if (excludedUrls.contains(request.getRequestURI())) {
+            return false;
+        }
+        return logger.isDebugEnabled();
+    }
 
     @Override
     protected void beforeRequest(HttpServletRequest request, String message) {
-        if (!request.getRequestURI().endsWith("/health")) {
-            logger.debug(message);
-        }
+        logger.debug(message);
     }
 
     @Override
     protected void afterRequest(HttpServletRequest request, String message) {
-        if (!request.getRequestURI().endsWith("/health")) {
-            logger.debug(message);
-        }
+        logger.debug(message);
     }
+
 }
